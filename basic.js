4594dca9089ef45504dd519c4672e3eff5db8104
brameldering <bram.eldering@gmail.com>
2023-08-06 18:56:15 +0200

    commit

---
+++ basic.js
@@ -0,0 +1,8 @@
+const Redis = require("ioredis");
+const redis = new Redis("localhost:6379");
+(async () => {
+  await redis.set("foo", "bar");
+  const result = await redis.get("foo");
+  console.log("result:", result);
+  redis.quit();
+})();
---
+++ transaction.js
@@ -0,0 +1,11 @@
+const Redis = require("ioredis");
+const redis = new Redis("localhost:6379");
+(async () => {
+  const [res_srem, res_hdel] = await redis
+    .multi()
+    .srem("employees", "42") // Remove from Set
+    .hdel("employee-42", "company-id") // Delete from Hash
+    .exec();
+  console.log("srem?", !!res_srem[1], "hdel?", !!res_hdel[1]);
+  redis.quit();
+})();
